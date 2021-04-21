384010a41e0518bb683a143d388f379342222fe6
Willi Zschiebsch <willi.zschiebsch@digifors.de>
2021-04-21 20:46:37 +0200

    erase pycache

--- dfgraph/__init__.py
+++
@@ -1,1 +0,0 @@
-from dfgraph.database import *
--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
-from setuptools import setup, find_packages
+import setuptools
 
-from dfgraph import __version__
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-setup(
-    dname='dfgraph',
-    version=__version__,
-
-    url='https://github.com/willi-z/dfgraph',
-    author='Willi Zschiebsch',
-    author_email='willi.w.zschiebsch@web.de',
-
-    packages = find_packages(),
-    
+setuptools.setup(
+    name="dfgraph", # Replace with your own username
+    version="0.0.1",
+    author="Willi Zschiebsch",
+    author_email="willi.w.zschiebsch@web.de",
+    description="A simple graph database system.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/willi-z/dfgraph",
+    project_urls={
+        "Bug Tracker": "https://github.com/willi-z/dfgraph/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
 )
